ed5901ee25a43bf469981b8dd9dc5da19cc4b5c8
mjbba-iscteiulpt <mjbba@iscte-iul.pt>
2018-05-30 19:04:35 +0100

    Saves last text

--- app/src/main/java/inducesmile/com/sid/App/LoginActivity.java
+++ app/src/main/java/inducesmile/com/sid/App/LoginActivity.java
@@ -1,11 +1,14 @@
 package inducesmile.com.sid.App;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import inducesmile.com.sid.Helper.UserLogin;
 import inducesmile.com.sid.R;
@@ -13,10 +16,21 @@ import inducesmile.com.sid.R;
 public class LoginActivity extends AppCompatActivity {
     private EditText ip, port,username,password;
     private Button login;
+    private SharedPreferences myPrefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        myPrefs = getSharedPreferences("prefID", Context.MODE_PRIVATE);
+
+        String ipText = myPrefs.getString("ip","");
+        String portText = myPrefs.getString("port","");
+
+        TextView labelIp = (TextView) findViewById(R.id.ip);
+        labelIp.setText(ipText);
+        TextView labelPort = (TextView) findViewById(R.id.port);
+        labelPort.setText(portText);
 
         ip = findViewById(R.id.ip);
         port = findViewById(R.id.port);
@@ -26,6 +40,12 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginClick(View v){
+
+        myPrefs = getSharedPreferences("prefID", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = myPrefs.edit();
+        editor.putString("ip", ip.getText().toString());
+        editor.putString("port", port.getText().toString());
+        editor.apply();
 
         new UserLogin(ip.getText().toString(), port.getText().toString(),username.getText().toString(),password.getText().toString());
         Intent i = new Intent(this, MainActivity.class);
