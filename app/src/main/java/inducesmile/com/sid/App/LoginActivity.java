cabc7ea84ac30f0ef291d25310830a7d7a2a1b37
mjbba-iscteiulpt <mjbba@iscte-iul.pt>
2018-06-02 22:48:13 +0100

    login funciona

--- app/src/main/java/inducesmile/com/sid/App/LoginActivity.java
+++ app/src/main/java/inducesmile/com/sid/App/LoginActivity.java
@@ -4,11 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import inducesmile.com.sid.Helper.UserLogin;
 import inducesmile.com.sid.R;
@@ -42,19 +51,39 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginClick(View v) {
+        URL url;
+        String response = null;
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            url = new URL("http://192.168.1.76/testConnection.php?user="+username.getText().toString()+"&pass="+password.getText().toString());
+            URLConnection conn = url.openConnection();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
 
+            while ((line = rd.readLine()) != null) {
+                response = line;
+            }
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Log.d("login", response);
+        if(response.contains("Connected")) {
+            myPrefs = getSharedPreferences("prefID", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = myPrefs.edit();
+            editor.putString("ip", ip.getText().toString());
+            editor.putString("port", port.getText().toString());
+            editor.apply();
 
-        myPrefs = getSharedPreferences("prefID", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = myPrefs.edit();
-        editor.putString("ip", ip.getText().toString());
-        editor.putString("port", port.getText().toString());
-        editor.apply();
-
-        new UserLogin(ip.getText().toString(), port.getText().toString(), username.getText().toString(), password.getText().toString());
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
-        finish();
-
+            new UserLogin(ip.getText().toString(), port.getText().toString(), username.getText().toString(), password.getText().toString());
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 
 
